6f4011808d68a4efdb00d7c695f330dafba9bb2e
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-01 17:54:39 +0500

    Application Project : Add BlogVm.cs in Queries/GetBlogs/BlogVm.cs => Id, Name, Description, Author; GetBlogs/GetBlogQuery : inherit from IRequest<List<BlogVm>>; GetBlogs/GetBlogQueryHandler : inherit from IRequestHandler<GetBlogQuery, List<BlogVm>> and Method Handler;

---
+++ CleanArchWithCQRSandMediator.Application/Blogs/Queries/GetBlogs/GetBlogQueryHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchWithCQRSandMediator.Application.Blogs.Queries.GetBlogs
+{
+    public class GetBlogQueryHandler : IRequestHandler<GetBlogQuery, List<BlogVm>>
+    {
+        public Task<List<BlogVm>> Handle(GetBlogQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
