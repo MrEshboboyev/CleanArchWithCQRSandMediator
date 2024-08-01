a0c4303cca203ff446eac3c813ad131caac507fc
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-01 22:04:43 +0500

    Application project : Blogs/Commands/CreateBlogCommand.cs => Name, Author, Description; CreateBlogCommandHandler.cs  => Handle() method returning created blog;

---
+++ CleanArchWithCQRSandMediator.Application/Blogs/Commands/CreateBlog/CreateBlogCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using CleanArchWithCQRSandMediator.Application.Blogs.Queries.GetBlogs;
+using CleanArchWithCQRSandMediator.Domain.Entity;
+using CleanArchWithCQRSandMediator.Domain.Repository;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchWithCQRSandMediator.Application.Blogs.Commands.CreateBlog
+{
+    public class CreateBlogCommandHandler : IRequestHandler<CreateBlogCommand, BlogVm>
+    {
+        private readonly IBlogRepository _blogRepository;
+        private readonly IMapper _mapper;
+
+        public CreateBlogCommandHandler(IBlogRepository blogRepository, IMapper mapper)
+        {
+            _blogRepository = blogRepository;
+            _mapper = mapper;
+        }
+        public async Task<BlogVm> Handle(CreateBlogCommand request, CancellationToken cancellationToken)
+        {
+            var blogEntity = new Blog() { Name = request.Name, Author = request.Author, Description = request.Description };
+
+            var result = await _blogRepository.CreateAsync(blogEntity);
+
+            return _mapper.Map<BlogVm>(result);
+        }
+    }
+}
