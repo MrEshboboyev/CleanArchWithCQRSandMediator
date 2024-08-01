441e619d62caa8a16de02c23c68caedcd02a0fe6
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-02 00:16:17 +0500

    BlogController : GetByIdAsync() action writed

--- CleanArchWithCQRSandMediator.API/Controllers/BlogController.cs
+++ CleanArchWithCQRSandMediator.API/Controllers/BlogController.cs
@@ -1,4 +1,5 @@
-﻿using CleanArchWithCQRSandMediator.Application.Blogs.Queries.GetBlogs;
+﻿using CleanArchWithCQRSandMediator.Application.Blogs.Queries.GetBlogById;
+using CleanArchWithCQRSandMediator.Application.Blogs.Queries.GetBlogs;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,6 +15,18 @@ namespace CleanArchWithCQRSandMediator.API.Controllers
             var blogs = await Mediator.Send(new GetBlogQuery());
 
             return Ok(blogs);
+        }
+        
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdAsync(int id)
+        {
+            var blog = await Mediator.Send(new GetBlogByIdQuery() { BlogId = id });
+
+            if(blog == null)
+            {
+                return NotFound();
+            }
+            return Ok(blog);
         }
     }
 }
