b76123ed03019ab286cccb3afe567dc943661b60
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-01 18:52:43 +0500

    GetBogQueryHandler.cs : DI for IBlogRepository, Handle() method from Get All Blogs wity Type "BlogVm";

--- CleanArchWithCQRSandMediator.Application/Blogs/Queries/GetBlogs/GetBlogQueryHandler.cs
+++ CleanArchWithCQRSandMediator.Application/Blogs/Queries/GetBlogs/GetBlogQueryHandler.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using CleanArchWithCQRSandMediator.Domain.Repository;
+using MediatR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +10,22 @@ namespace CleanArchWithCQRSandMediator.Application.Blogs.Queries.GetBlogs
 {
     public class GetBlogQueryHandler : IRequestHandler<GetBlogQuery, List<BlogVm>>
     {
-        public Task<List<BlogVm>> Handle(GetBlogQuery request, CancellationToken cancellationToken)
+        private readonly IBlogRepository _blogRepository;
+
+        public GetBlogQueryHandler(IBlogRepository blogRepository)
         {
-            throw new NotImplementedException();
+            _blogRepository = blogRepository;
+        }
+
+        public async Task<List<BlogVm>> Handle(GetBlogQuery request, CancellationToken cancellationToken)
+        {
+            var blogs = await _blogRepository.GetAllBlogsAsync();
+            
+            // convert blog to blogVm
+            var blogList = blogs.Select(bg => new BlogVm { Id = bg.Id, Author = bg.Author, 
+                Description = bg.Description, Name = bg.Name }).ToList();
+
+            return blogList;
         }
     }
 }
